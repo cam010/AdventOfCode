ad03e1c85f5c2567f814c4fe1891aab938907bb9
Cameron Clarke <cameron.clarke@student.manchester.ac.uk>
2025-12-03 01:43:48 +0000

    Completed Day1 Part 1. Next moving to part 2

---
+++ 2025/day1/part1.py
@@ -0,0 +1,42 @@
+# Password is how many times dial shows zero during turn combinations.
+# Dial is circular
+# INPUT FORMAT: file, each line:
+#                               LNUM || RNUM
+# If LNUM -> "rotate" dial left NUM times. If RNUM -> "rotate dial right NUM times"
+
+# NOTES
+# Iterate through file, Sum NUMs together, if L then add negative NUM.
+# After every "turn" instruction -> if total % 100 = 0 then count ++
+# Count is password.
+
+# ANSWER - 1180 (Correct)
+
+
+import os
+
+
+DIAL_SIZE = 100
+FILENAME = os.path.join(os.path.dirname(__file__), "input.txt")
+
+total = 50
+count = 0
+
+with open(FILENAME, "r") as f:
+    while True:
+        line = f.readline()
+        
+        if line == "":
+            # End of File
+            break
+    
+        # No need for input verification
+        num = int(line[1:])  # first character is L/R
+        if line.startswith("L"):
+            num = -num
+    
+        total += num
+        if total % DIAL_SIZE == 0:
+            count += 1
+
+pwd = count
+print(pwd)
