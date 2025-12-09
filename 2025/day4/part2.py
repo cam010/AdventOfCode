38d9c7cef44226964336ec7418b2075791c2efce
Cameron Clarke <cameron.clarke@student.manchester.ac.uk>
2025-12-09 14:20:34 +0000

    Completed Day 4

---
+++ 2025/day4/part2.py
@@ -0,0 +1,63 @@
+import os
+
+FILENAME = os.path.join(os.path.dirname(__file__), "input.txt")
+NUMCOUNT = 12
+
+with open(FILENAME, "r") as f:
+    grid = []
+    grid = [row for row in f.read().split("\n")]
+
+cols = len(grid[0])       # How many cols in a row
+rows = len(grid)          # How many rows there are
+
+total = 0
+changed = 1
+
+while changed:
+    changed = 0
+    new_grid = [col for col in [row for row in grid]]
+    for i in range(rows):   # Rows
+        new_row = ""
+        for j in range(cols):  # Colums
+            if grid[i][j] != "@":   # If grid is not @ then we can skip this gridsquare
+                new_row += "."
+                continue
+            if (i==0 and j==0) or (i==rows-1 and j==0) or (i==0 and j==cols-1) or (i==rows-1 and j==cols-1):
+                # We are at a corner - cannot have >= 4 adjacent @ symbols because only 3 adjacent grid squares so always valid.
+                total += 1
+                new_row += "x"
+                continue
+            
+            count = 0
+            
+            if i != 0:  # Check (up to) 3 above as we are not checking the top row
+                if grid[i-1][j] == "@":     # N square
+                    count += 1
+                if (j != 0) and (grid[i-1][j-1]) == "@": # NW square
+                    count += 1 
+                if (j != cols-1) and grid[i-1][j+1] == "@":   # NE square
+                    count += 1
+            if i != rows-1: # Check (up to) 3 below as we are not checking the top row
+                if grid[i+1][j] == "@":     # S square
+                    count += 1
+                if (j != 0) and (grid[i+1][j-1]) == "@": # SW square
+                    count += 1 
+                if (j != cols-1) and grid[i+1][j+1] == "@":   # SE square
+                    count += 1
+            if j != 0:
+                if grid[i][j-1] == "@": # W square
+                    count += 1
+            if j != cols-1:
+                if grid[i][j+1] == "@": # E square
+                    count += 1
+            if count < 4:
+                total += 1
+                new_row += "x"
+                changed = 1
+            else:
+                new_row += "@"
+        new_grid[i] = new_row
+    grid = new_grid
+        
+    
+print(total)
